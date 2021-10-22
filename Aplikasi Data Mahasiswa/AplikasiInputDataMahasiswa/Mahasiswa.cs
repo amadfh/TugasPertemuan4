73ee3545d34d25894138f405dca6be2e349bfca5
amadfh <91525165+amadfh@users.noreply.github.com>
2021-10-22 10:32:12 +0700

    upload tugad

---
+++ Aplikasi Data Mahasiswa/AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,11 @@
+﻿namespace AplikasiInputDataMahasiswa
+{
+    internal class Mahasiswa
+    {
+        public string Nim;
+        public string Nama;
+        public string Kelas;
+        public int Nilai;
+        public string NilaiHuruf;
+    }
+}
